91ee2bd455921751ee1f11b6ad5a691b3cf53cca
KDawid <kdawid93@gmail.com>
2019-05-28 19:19:28 +0200

    Ubuntu setup script alpha version added

---
+++ ubuntu_setup/install.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+echo Installing cowsay...
+sudo apt-get install cowsay
+
+echo Installing sl...
+sudo apt-get install sl
+
+echo Installing htop...
+sudo apt-get install htop
+
+echo Installing vim...
+sudo apt-get install vim
+
+echo Installing git...
+sudo apt-get install git
+
+echo Installing gitk...
+sudo apt-get install gitk
+
+echo Installing vlc...
+sudo apt-get install vlc
+
+echo Installing maven...
+sudo apt-get install maven
+
+echo Installing geany...
+sudo apt-get install geany
+
+echo Installing openssh...
+sudo apt-get install openssh-client
+sudo apt-get install openssh-server
+
+echo Update...
+sudo apt-get update
+
+echo Done.
